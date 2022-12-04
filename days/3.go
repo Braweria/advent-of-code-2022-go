eedb3ceddb645768053acde691e0b29912d800a2
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2022-12-04 11:54:13 +0100

    solve day 3 part 2

--- days/3.go
+++ days/3.go
@@ -14,20 +14,30 @@ func D03() {
 	scanner := bufio.NewScanner(file)
 	sum := 0
 
+	badgeSum := 0
+	group := []string{}
+
 	for scanner.Scan() {
 		line := scanner.Text()
 
 		compartments := splitInTwo(line)
-
 		duplicate := getDuplicateItem(compartments[0], compartments[1])
 		runeDupe := []rune(duplicate)[0]
-
 		prio := getPriority(runeDupe)
 
 		sum += prio
+
+		group = append(group, line)
+		if len(group) == 3 {
+			badge := getBadge(group)
+			badgePrio := getPriority(badge)
+			badgeSum += badgePrio
+
+			group = []string{}
+		}
 	}
 
-	fmt.Println("Day 03", sum)
+	fmt.Println("Day 03", sum, badgeSum)
 }
 
 func splitInTwo(line string) []string {
@@ -50,4 +60,29 @@ func getPriority(char rune) int {
 	default:
 		return int(char-'A') + 27
 	}
+}
+
+func getBadge(group []string) rune {
+
+	firstBag := group[0]
+	secondBag := group[1]
+	thirdBag := group[2]
+
+	for _, item := range firstBag {
+		if includesElement(secondBag, item) {
+			if includesElement(thirdBag, item) {
+				return item
+			}
+		}
+	}
+	panic("No badge found")
+}
+
+func includesElement(arr string, element rune) bool {
+	for _, item := range arr {
+		if item == element {
+			return true
+		}
+	}
+	return false
 }
